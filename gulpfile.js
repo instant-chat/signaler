4b3695b01839dbd88faeeab28f3733551457bc87
Blake La Pierre <blakelapierre@gmail.com>
2015-03-26 00:44:42 -0700

    Add test task

--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,7 @@ gulp.task('build', sequence('clean', 'runtime'));
 gulp.task('dev', ['runtime'], () => gulp.watch(paths.scripts, ['runtime']));
 
 gulp.task('run', () => run(`node ${paths.dist}/index.js ${args.args || ''}`).exec());
+gulp.task('test', () => run(`node ${paths.dist}/tests/index.js ${args.args || ''}`).exec());
 
 gulp.task('transpile', //['jshint'],
   () => pipe([
